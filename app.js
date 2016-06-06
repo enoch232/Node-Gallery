9cd0eb90ef61b2320351207cc5c2369228eed7c8
enoch232 <enoch232@gmail.com>
2016-06-05 23:51:34 -0700

    needs fixing for galleries or users

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 var express = require('express');
 var multer = require('multer');
 var mongoose = require('mongoose');
+var Gallery = require('./models/user')
+mongoose.connect("mongodb://localhost/nodegallery");
 var app = express();
 
 var port = process.env.PORT || 3000;
 
 app.get("/",function(req, res){
-	res.end("Node Gallery");
+	Gallery.getGalleries(function(err, galleries){
+		if (err){
+			res.json(err);
+		}else{
+			res.json(galleries);
+		}
+	});
 });
-app.post("/", function(req, res){
-
-});
-
-app.listen(port);
 console.log("Server is now running at port "+port);
+app.listen(port);
