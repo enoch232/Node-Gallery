9e1181b5e7ee998dad54433060161df133fa782d
enoch232 <enoch232@gmail.com>
2016-06-05 23:27:00 -0700

    add dependencies and change port

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 var express = require('express');
+var multer = require('multer');
+var mongoose = require('mongoose');
 var app = express();
+
+var port = process.env.PORT || 3000;
+
 app.get("/",function(req, res){
 	res.end("Node Gallery");
 });
-app.listen(3000);
-console.log("Server is now running at port 3000");
+app.post("/", function(req, res){
+
+});
+
+app.listen(port);
+console.log("Server is now running at port "+port);
